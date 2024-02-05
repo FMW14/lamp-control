f3a9716ed512ca2175a42abb9a263fd2c3273126
FMW14 <fmw14@yandex.ru>
2024-02-05 14:00:33 +0300

    Initial commit

---
+++ src/main/java/ru/bjartskular/lampcontrol/LampControlApplication.java
@@ -0,0 +1,13 @@
+package ru.bjartskular.lampcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LampControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LampControlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/bjartskular/lampcontrol/LampControlApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bjartskular.lampcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LampControlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
